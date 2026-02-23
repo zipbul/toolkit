58a15f21a940164f0370a10964a80664184a54ba
parkrevil <revil.com@gmail.com>
2026-02-24 00:46:01 +0900

    chore(baker): remove backer package

--- packages/baker/src/seal/index.ts
+++
@@ -1,5 +0,0 @@
-export { seal, _resetForTesting, mergeInheritance } from './seal';
-export { buildDeserializeCode } from './deserialize-builder';
-export { buildSerializeCode } from './serialize-builder';
-export { analyzeCircular } from './circular-analyzer';
-export { validateExposeStacks } from './expose-validator';
--- packages/baker/src/stubs/date.ts
+++
@@ -1,6 +0,0 @@
-import type { ValidationOptions } from '../interfaces';
-
-const noop: PropertyDecorator = () => {};
-
-export function MinDate(_date: Date, _options?: ValidationOptions): PropertyDecorator { return noop; }
-export function MaxDate(_date: Date, _options?: ValidationOptions): PropertyDecorator { return noop; }
